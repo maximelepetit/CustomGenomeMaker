f65c5e6ece302217cf4f819039dafa3b55de6956
Maxime LEPETIT <maxime.lepetit@univ-lyon1.fr>
2025-02-27 14:25:04 +0100

    Add test data

---
+++ tests/testscript/script.sh
@@ -0,0 +1,23 @@
+bash HybridGenomeMaker.sh \
+ -sequence_file "/home/labex-cortex/bioinfo-pf-cortex/dev/data/test.fa" \
+ -speciesName "caenorhabditis_elegans" \
+ -outDir "/mnt/Data1/genomes"
+
+ bash HybridGenomeMaker.sh \
+ -sequence_file "/home/labex-cortex/bioinfo-pf-cortex/dev/data/dsRed.txt" \
+ -speciesName "caenorhabditis_elegans" \
+ -outDir "/mnt/Data1/genomes"
+
+
+
+  bash HybridGenomeMaker.sh  -sequence_file "/home/labex-cortex/bioinfo-pf-cortex/dev/data/test.fa" -speciesName "caenorhabditis_elegans" -outDir "/mnt/Data1/genomes"  -fasta  "/mnt/Data1/genomes/fasta/Caenorhabditis_elegans/Caenorhabditis123_elegans.WBcel235.dna.toplevel.fa.gz" -gtf "/mnt/Data1/genomes/gtf/Caenorhabditis_elegans/Caenorhabditis123_elegans.WBcel235.113.gtf.gz"
+ 
+
+
+  bash HybridGenomeMaker.sh \
+ -sequence_file "/home/labex-cortex/bioinfo-pf-cortex/dev/data/dsRed.txt" \
+ -speciesName "caenorhabditis_elegans" \
+ -outDir "/mnt/Data1/genomes" \
+ -fasta  "/mnt/Data1/genomes/fasta/Caenorhabditis_elegans/Caenorhabditis_elegans.WBcel235.dna.toplevel.fa.gz" \
+ -gtf "/mnt/Data1/genomes/gtf/Caenorhabditis_elegans/Caenorhabditis_elegans.WBcel235.113.gtf.gz"
+
